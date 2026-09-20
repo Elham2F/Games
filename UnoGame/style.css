adf6c31bf11ee06aa8776586fafdc4f25fb1f0a3
Elham <elhamalf@outlook.com>
2026-09-20 06:26:27 +0300

    Create style.css

---
+++ UnoGame/style.css
@@ -0,0 +1,18 @@
+const colors = ['أخضر', 'رملي', 'طيني', 'بحري'];
+const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'ديور', 'تعدي', '+2'];
+let deck = [];
+
+// وظيفة لإنشاء مجموعة الأوراق كاملة
+function buildDeck() {
+    for (let i = 0; i < colors.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push({ color: colors[i], value: values[j] });
+            // في الأونو، الأرقام من 1 إلى 9 والأكشن تتكرر مرتين لكل لون
+            if (values[j] !== '0') {
+                deck.push({ color: colors[i], value: values[j] });
+            }
+        }
+    }
+    // إضافة بطاقات تغيير اللون (4 بطاقات) و +4 (4 بطاقات)
+    // ... سيتم إضافتها هنا
+}
